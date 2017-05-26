7a4aabfc2bb14da26c2c13677d30f2ea9ccd4019
Eidolon <furyhunter600@gmail.com>
2017-05-26 18:53:17 -0500

    Add more robust schema support

--- src/struct/struct.test.js
+++ src/struct/struct.test.js
@@ -1,59 +1,56 @@
 import * as struct from '.';
 
+function doTest(schema, array, expected, registries = []) {
+  test(`schema ${schema} parses buffer to ${expected}`, () => {
+    let buf = Uint8Array.from(array);
+    let parsed = struct.parse(buf.buffer, schema, registries);
+    if (typeof expected === 'number') {
+      expect(parsed).toBeCloseTo(expected);
+    } else {
+      expect(parsed).toEqual(expected);
+    }
+  });
+}
+
 describe('struct parser', () => {
-  test('simple schema parses u8 buffer', () => {
-    let schema = {
-      val: 'u8',
-    };
-    let buf = Uint8Array.of(0x04);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val: 4});
+  describe('primitives', () => {
+    doTest('u8', [4], 4);
+    doTest('u16', [0, 4], 4);
+    doTest('u32', [0, 0, 0, 4], 4);
+    doTest('u16le', [4, 0], 4);
+    doTest('u32le', [4, 0, 0, 0], 4);
+    doTest('i8', [255], -1);
+    doTest('i16', [255, 254], -2);
+    doTest('i32', [255, 255, 255, 254], -2);
+    doTest('i16le', [254, 255], -2);
+    doTest('i32le', [254, 255, 255, 255], -2);
+
+    doTest('f32', [64, 131, 51, 51], 4.1);
+    doTest('f32le', [51, 51, 131, 64], 4.1);
+    doTest('f64', [102, 102, 102, 102, 102, 102, 16, 64].reverse(), 4.1);
+    doTest('f64le', [102, 102, 102, 102, 102, 102, 16, 64], 4.1);
   });
 
-  test('simple schema parses u16be buffer', () => {
-    let schema = {
-      val: 'u16',
-    };
-    let buf = Uint8Array.of(0x00, 0x04);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val: 4});
+  // Actual schemas
+  describe('object schemas', () => {
+    doTest({val: 'u8'}, [4], {val: 4});
+    doTest({val: {val2: 'u8'}}, [4], {val: {val2: 4}});
+    doTest({val: {val2: {val3: 'u8'}}}, [4], {val: {val2: {val3: 4}}});
+    doTest({val: {val2: 'i8'}, val2: 'i8'}, [4, 255], {val: {val2: 4}, val2: -1});
   });
 
-  test('simple schema parses u32be buffer', () => {
-    let schema = {
-      val: 'u32',
+  describe('complex schemas', () => {
+    let func = (cursor, registry) => {
+      let size = struct.parseWithCursor(cursor, 'u8', registry);
+      let vals = [];
+      for (let i = 0; i < size; i++) {
+        vals.push(struct.parseWithCursor(cursor, 'u8', registry));
+      }
+      return vals;
     };
-    let buf = Uint8Array.of(0x00, 0x00, 0x00, 0x04);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val: 4});
-  });
 
-  test('simple schema parses two u8s', () => {
-    let schema = {
-      val1: 'u8',
-      val2: 'u8',
-    };
-    let buf = Uint8Array.of(0x01, 0x02);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val1: 1, val2: 2});
-  });
-
-  test('simple schema parses two u16bes', () => {
-    let schema = {
-      val1: 'u16',
-      val2: 'u16',
-    };
-    let buf = Uint8Array.of(0x00, 0x01, 0x00, 0x02);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val1: 1, val2: 2});
-  });
-
-  test('simple schema parses i8', () => {
-    let schema = {
-      val: 'i8',
-    };
-    let buf = Uint8Array.of(0xFF);
-    let parsed = struct.parse(buf.buffer, schema);
-    expect(parsed).toEqual({val: -1});
+    doTest(func, [2, 0, 1], [0, 1]);
+    doTest(func, [4, 0, 0, 0, 1], [0, 0, 0, 1]);
+    doTest(func, [0], []);
   });
 });
